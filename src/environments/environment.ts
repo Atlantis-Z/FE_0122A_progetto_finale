8d50e6fa27002bda72734de1e2e699e12717a9b9
Atlantis-Z <pirecizyber@gmail.com>
2022-04-22 09:53:53 +0200

    Progetto finito

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   pathApi: 'http://epicode.online/epicodebeservice_v2',
   adminToken: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY1MDYxMzE3MywiZXhwIjoxNjUyNzYwNjU3fQ.c4acV5QLQzuKK0IVjsRpM2JIo6BMBSEEQdGtqaLk3mICK_Mdo5qDzpuWJQIZCbrnmUMHnmtLQ6hlnSylLzJWLg',
+
   adminTenant:'fe_0122a'
 }
 
